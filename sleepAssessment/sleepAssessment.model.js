a38a9a9f2448a5a273c2c83d8d661f516f53d5ee
ahaan-parmar <ahaanparmar6@gmail.com>
2025-07-09 01:47:10 +0530

    minor fixes

--- sleepAssessment/sleepAssessment.model.js
+++ sleepAssessment/sleepAssessment.model.js
@@ -1,1 +1,33 @@
- 
+const mongoose = require('mongoose');
+
+const sleepAssessmentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true // Only one assessment per user for MVP
+  },
+  durationStruggling: {
+    type: String,
+    enum: ['<2 weeks', '2-8 weeks', '>8 weeks'],
+    required: true
+  },
+  bedtime: {
+    type: String,
+    required: true
+  },
+  waketime: {
+    type: String,
+    required: true
+  },
+  typicalHours: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('SleepAssessment', sleepAssessmentSchema); 
